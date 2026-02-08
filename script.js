58c902e52083c7fac8de0b0076720a9248e9f913
duniaklf2000-Hub <duniaklf2000@gmail.com>
2026-02-09 00:34:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function setLang(lang) {
+  document.querySelectorAll('[data-lang]').forEach(el => {
+    el.style.display =
+      el.getAttribute('data-lang') === lang ? 'block' : 'none';
+  });
+
+  document.body.dir = (lang === 'ar') ? 'rtl' : 'ltr';
+}
+
+// Standardsprache
+setLang('de');
